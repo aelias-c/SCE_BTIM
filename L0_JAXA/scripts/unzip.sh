836c1ee877df9d52a4984e6586d6668eab3bcc95
aelias-c <66627234+aelias-c@users.noreply.github.com>
2025-06-20 15:23:26 -0400

    Update unzip.sh

--- L0_JAXA/scripts/unzip.sh
+++ L0_JAXA/scripts/unzip.sh
@@ -3,3 +3,5 @@ find $HOME/L0_JAXA/weekly -name "*.hdf" > all_hdf_files.txt
 
 cat all_hdf_files.txt | parallel --wd $PWD -j 40 python ./process_single_hdf.py {}
 
+
+cdo mergetime $HOME/L0_JAXA/scripts/weekly_processed/surface_flag.{match}.nc $HOME/L0_JAXA/JAXA.JASMES.sce.nc
