d931126c096bd900bbdf071eb8d064f63a936148
aelias-c <66627234+aelias-c@users.noreply.github.com>
2025-06-20 14:46:13 -0400

    Update regrid_fast.sh

--- L0_Regridding/regrid_fast.sh
+++ L0_Regridding/regrid_fast.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-REF_GRID=/users/jk/22/snow2024/Paper2/L0_Reference/NSIDC_EASE2_N25km_v04.nc
+REF_GRID=$HOME/L0_Reference/NSIDC_EASE2_N25km_v04.nc
 
 MC=${1} # first argument, modelling centre
 MODEL=${2} # second argument, model
 
 # Create new directory if one doesn't exist for the model
-mkdir -p /users/jk/22/achereque/Paper2/L0_Regridding/${MC}/${MODEL}
+mkdir -p $HOME/${MC}/${MODEL}
 
 # Directories
-DATA_LOC=/users/jk/22/achereque/Paper2/L0_BTIM/${MC}/${MODEL}
-OUTLOC=/users/jk/22/achereque/Paper2/L0_Regridding/${MC}/${MODEL}
+DATA_LOC=$HOME/L0_BTIM/${MC}/${MODEL}
+OUTLOC=$HOME/L0_Regridding/${MC}/${MODEL}
 
 ### Conservative regridding
 # list files and then, in parallel, remap with first order conservative method
